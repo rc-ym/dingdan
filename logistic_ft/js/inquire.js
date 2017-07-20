838cd52d318ad095b2e1f74a7b4e2a0afb34af79
tenkiss258 <837442643@qq.com>
2017-07-20 17:58:37 +0800

    cc

--- logistic_ft/js/inquire.js
+++ logistic_ft/js/inquire.js
@@ -37,4 +37,39 @@ $(function(){
             }
         }
     })
+    search_text.children('input').keyup(function(e){
+        var event=e||window.event;
+        if (event.keyCode == 13) {
+            if(search_text.children('input').val()==''){
+                search_alert.html("不能为空");
+                search_alert.css("top",0);
+                setTimeout(function(){
+                    search_alert.css("top","-100%");
+                },3000)
+            }else{
+                if (isNaN(search_text.children('input').val())){
+                    search_alert.html("必须为数字");
+                    search_alert.css("top",0);
+                    setTimeout(function(){
+                        search_alert.css("top","-100%");
+                    },3000)
+                }else{
+                    if(search_text.children('input').val().length==12){
+                        jiaz.css('display','block');
+                        setTimeout(function(){
+                            jiaz.css('display','none');
+                            content.css('display','block');
+                        },2000)
+
+                    }else{
+                        search_alert.html("请输入12位订单号号、运单号");
+                        search_alert.css("top",0);
+                        setTimeout(function(){
+                            search_alert.css("top","-100%");
+                        },3000)
+                    }
+                }
+            }
+        };
+    })
 })
