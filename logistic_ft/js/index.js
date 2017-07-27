a0dba1ef8f2af5d4e69e9ba9f6767423c6c9e9b4
tenkiss258 <837442643@qq.com>
2017-07-27 09:57:14 +0800

    '添加了二级三级菜单'

--- logistic_ft/js/index.js
+++ logistic_ft/js/index.js
@@ -60,39 +60,4 @@ $(function(){
             }
         }
     })
-    search_text.children('textarea').keyup(function(e){
-        var event=e||window.event;
-         if(event.keyCode == 13){
-            if(search_text.children('textarea').val()==''){
-                search_alert.html("不能为空");
-                search_alert.css("bottom",0);
-                setTimeout(function(){
-                    search_alert.css("bottom","-100%");
-                },3000)
-            }else{
-                if (isNaN(search_text.children('textarea').val())){
-                    search_alert.html("必须为数字");
-                    search_alert.css("bottom",0);
-                    setTimeout(function(){
-                        search_alert.css("bottom","-100%");
-                    },3000)
-                }else{
-                    if(search_text.children('textarea').val().length==12){
-                        search_alert.html("跳转查询页面接口");
-                        search_alert.css("bottom",0);
-                    }else{
-                        search_alert.html("请输入12位订单号号、运单号");
-                        search_alert.css("bottom",0);
-                        setTimeout(function(){
-                            search_alert.css("bottom","-100%");
-                        },3000)
-                    }
-                }
-            }
-           search_text.children('textarea').blur();
-         } 
-    })
-
-
-
 })
